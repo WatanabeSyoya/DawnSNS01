f7ed5ccdf76d4a8e4a831942bd58d2c96397e311
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-06-17 20:34:26 +0900

    search

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/', 'PostsController@index')->name('posts.index');
 
 Route::get('/profile', 'UsersController@profile');
 
-//Route::get('/search', 'UsersController@index');
+Route::get('/search', 'UsersController@index');
 
 //Route::get('/follow-list', 'PostsController@index');
 //Route::get('/follower-list', 'PostsController@index');
